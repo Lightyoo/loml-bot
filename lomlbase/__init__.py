2193f8b940b1b7e79a744832a214081f0f0249a7
Light <mailllj@163.com>
2022-07-07 19:04:22 +0800

    be failed again

--- lomlbase/__init__.py
+++ lomlbase/__init__.py
@@ -15,7 +15,8 @@ class Words:
 	NOUNS = [
 		W(split="apple",complex="apples"),
 		W("pear"),
-		W(split="pig",complex="pigs")
+		W(split="pig",complex="pigs"),
+		W("world")
 		]
 
 	# pron.
@@ -35,7 +36,13 @@ class Words:
 		]
 
 	# adj.
-	ADJECTIVE = []
+	ADJECTIVE = [
+		W("nice"),
+		W("terrible"),
+		W("excited"),
+		W("quiet"),
+		W("rude")
+		]
 
 	# adv.
 	ADVERB = []
@@ -45,7 +52,8 @@ class Words:
 		W(split="eat",present="eating"),
 		W(split="love"),
 		W("buy"),
-		W("throw")
+		W("throw"),
+		W("hello")
 		]
 
 	################ FAKE WORD
@@ -56,30 +64,201 @@ class Words:
 		]
 
 	# prep.
-	PREPOSITION = []
+	PREPOSITION = [
+		W("on"),
+		W("in")
+		]
 
 
 	W_PROPERTY = ["split","antisense","complex","present"]
-	ALL_PROPERTIES = [NOUNS,PRONOUN,NUMERAL,ADJECTIVE,ADVERB,VERB,ARTICLE,PREPOSITION]
+	ALL_PROPERTIES = {
+		"n."   :  NOUNS,
+		"pron.":  PRONOUN,
+		"num." :  NUMERAL,
+		"adj." :  ADJECTIVE,
+		"adv." :  ADVERB,
+		"v."   :  VERB,
+		"art." :  ARTICLE,
+		"prep" :  PREPOSITION
+		}
 
 
-def format(string=""):
-	string = tuple(string.lower().split(" "))
+
+class Model:
+	def __init__(self):
+		self.ALL_MODEL = [
+			(r"$1v.split$ $1n.split$",r"$1v.split$ $1n.split$ ? ? ?"),
+			(r"$1v.split$ a $1n.split$",r"$1v.split$ $1n.split$")
+			]
+
+		'''
+		
+		'''
+		self._turn()
+
+
+	def _turn(self):
+		'''
+		cut the s_expression
+		and they will become some lists and tuples
+		it's easy to opearte
+		'''
+		def splited(s_expression):
+			pattern_s_expression = r"\$([0-9]+)(\w+\.)(\w+)\$"
+
+			return [list(i) for i in re.findall(pattern_s_expression,s_expression)]
+
+		for index,(s_expression,reply_s_expression) in enumerate(self.ALL_MODEL):
+			self.ALL_MODEL[index] = splited(s_expression),reply_s_expression
+
+
+	def match(self,deal_string,turned_model):
+		model_appearence = turned_model[0]
+		model_reply = turned_model[1]
+
+		print("")
+		print(deal_string)
+		print(model_appearence)
+
+		# above all
+		# judge whether the model_appearence's length is the same to deal_string's length
+		if model_appearence.__len__() == deal_string.__len__():
+
+			for index,fg in enumerate(deal_string):
+
+				if str(type(fg)) == "<class 'list'>":
+					for element in fg:
+						if element[1] == model_appearence[index][1] and element[2] == model_appearence[index][2]: 
+							model_appearence[index][2] = element[3]
+
+					if str(type(model_appearence[index][2])) == "<class 'str'>":
+						return False
+
+				elif str(type(fg)) == "<class 'str'>":
+					print("str type element")
+					return False
+
+		else:
+			print("the lengths are different!")
+			return False
+
+		return model_reply
+
+
+
+def format_glo(string=""):
+	string = tuple(string.split(" "))
 	# ("i","eat","an","apple")
 	
 	'''
 	the program will walk the whole tuple
 	'''
 
-	for word in string:
+	print(string)
+
+	result = []
+
+	for i in range(len(string)):
+		result.append([])
+
+	for index,word in enumerate(string):
 		# word -> i | eat | an | apple
 		# if word in the large words list , the program will turn the common word string into difficult WordObj
 		# for example(if we dont know what's 'apple')
 		# (i , eat , an ,apple)
-		# and after handling,it becomes ["i"]
+		# and after handling,it becomes [[("pron.","split",wobj),("pron.","antisense",wobj)]]
+
+		for property_str,word_list in Words.ALL_PROPERTIES.items():
+			# word_list -> many Wo(Wobject)
+			# property_str -> use string to express the word's property
+
+			for wo in word_list:
+				# the wo
+				# like W("nice")
+				# their all properties is included in Words.W_PROPERTY
+				for wo_pro in Words.W_PROPERTY:
+					# wo_pro -> the properties Wo includes
+
+					value = getattr(wo,wo_pro) # the value of the property of Wo
+
+					if word == value:
+						# we need to turn it into global appearence
+						# like ("pron.","split",wo)
+
+						'''
+						first {
+							pron. -> property_str
+
+						second{
+							split -> wo_pro
+
+						third{
+							wo    -> wo
+
+						'''
+						result[index].append([word,property_str,wo_pro,wo])
+						# word represents itself
+						# property_str represents its word's property 
+						# wo_pro represents its property in the Word Object
+						# wo represents its Word Object
+
+		if result[index] == []:
+			print(f"{word} is unknown,so I turn the list into string")
+			result[index] = word
+
+		elif result[index] != []:
+			print(f"'{word} it has {len(result[index])} meaning,so we create{result[index]}")
+
+	print("")
+
+	return result
+
+
+
+
+def auto(string):
+	# (1)
+	# keep the first data
+	# Like "i eat an apple"
+	# but we should lower
+	string = string.lower()
+	print(string,"\n")
+
+
+	# (2)
+	# turn the data into global format
+	# but global format is not fit to splited s_expression
+	deal_string = format_glo(string)
+	print("final result is:",deal_string,"\n")
+
+
+	# (3)
+	model = Model()
+	print("Loading all the models: ",model.ALL_MODEL)
+
+
+	# (4)
+	print(model.match(deal_string,model.ALL_MODEL[1]))
+	# the model I choose the first for now
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
-	string = "I eat an apple"
+	string = "Hello a world"
+	auto(string)
+
+
 
 
 
